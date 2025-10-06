c0eddfb48b542299a18ae93c6b92ac9d977364e2
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-10-06 12:14:07 +0545

    big o notation completed

---
+++ BigONotation/src/QuestionSix.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class QuestionSix {
+
+	public static void main(String[] args) {
+		QuestionSix six = new QuestionSix();
+		int[] array = { 1, 2, 3, 4, 5 };
+		six.reverseArray(array);
+		System.out.print(Arrays.toString(array));
+
+        // Time Complexity O(n)
+
+	}
+
+	public void reverseArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			int other = array.length - i - 1;
+			int temp = array[i];
+			array[i] = array[other];
+			array[other] = temp;
+		}
+
+	}
+
+}
---
+++ BigONotation/src/QuestionTwo.java
@@ -0,0 +1,22 @@
+public class QuestionTwo {
+
+	public static void main(String[] args) {
+		
+		QuestionTwo two = new QuestionTwo();
+		int[] array = {1,2,3};;
+		two.printPair(array);
+
+        // Time Complexity O(N2)
+		
+
+	}
+	
+	public void printPair(int[] array) {
+		for(int i=0; i<array.length;i++) {
+			for(int j=0; j<array.length;j++) {
+				System.out.println(array[i]+","+array[j]);
+			}
+		}
+	}
+
+}
