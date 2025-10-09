a8b455330c9dfbeeffdd570ca6100345854bbecd
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-10-09 12:40:37 +0545

    basic tree creation done

---
+++ Binary Tree/src/Main.java
@@ -0,0 +1,25 @@
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        TreeNode drinks = new TreeNode("Drinks");
+        TreeNode hot = new TreeNode("Hot");
+        TreeNode cold = new TreeNode("Cold");
+        drinks.addChild(hot);
+        drinks.addChild(cold);
+
+        TreeNode tea = new TreeNode("Tea");
+        TreeNode coffee = new TreeNode("Coffee");
+        hot.addChild(tea);
+        hot.addChild(coffee);
+
+        TreeNode wine = new TreeNode("Wine");
+        TreeNode beer = new TreeNode("Beer");
+        cold.addChild(wine);
+        cold.addChild(beer);
+
+        System.out.println(drinks.print(0));
+    }
+}
---
+++ Binary Tree/src/TreeNode.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode {
+
+    public String data;
+    public List<TreeNode> children;
+
+    public TreeNode(String data){
+        this.data = data;
+        children = new ArrayList<>();
+    }
+
+    public void addChild(TreeNode node){
+        this.children.add(node);
+    }
+
+    public String print(int level) {
+        String ret;
+        ret = " ".repeat(level)+this.data+"\n";
+        for(TreeNode node : this.children) {
+            ret += node.print(level+1);
+        }
+        return ret;
+    }
+}
