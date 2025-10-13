09e6c2d971ab9bd3d44d924d03e056caade9dc1d
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-10-13 12:30:12 +0545

    trie implementation completed with linked list

---
+++ Trie/src/TrieMain.java
@@ -0,0 +1,10 @@
+
+public class TrieMain {
+    public static void main(String[] args) {
+      Trie trie = new Trie();
+      trie.insert("API");
+      trie.insert("APIS");
+      trie.search("API");
+      trie.delete("API");
+    }
+}
